66af962e2452d23e3d05e3574cdb786b1fa9a195
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2021-11-04 01:50:16 +0300

    Connect Add, Remove and get book with api using redux

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useSelector } from 'react-redux';
 import BooksList from './BooksList.js';
 import BookAdd from './BookAdd.js';
+import { getBooks } from '../redux/books/books';
 
 const Books = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
+
+  const BookItem = [];
   const booklist = useSelector((data) => data.booksReducer.books);
-  const BookItem = booklist.map((item) => (
-        <BooksList title = {item.title} Author = {item.author} id ={item.id}/>
-  ));
+  Object.entries(booklist).forEach((element) => {
+    const [key, value] = element;
+    BookItem.push(value.map((item) => (
+      <BooksList
+       title = {item.title}
+       Author = {item.title}
+       category = {item.category}
+       id ={key}/>
+    )));
+  });
+
   return (
         <div>
             {BookItem}
